66f882b2cc14c8dc0a78f381b8b63c98febdc8b0
JustThz <JustThz@hotmail.com>
2016-03-24 12:06:30 -0600

    3.06 Lanzar SettingsActivity: Se lanza desde MainActivity y DetailActivity

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -39,9 +39,9 @@ public class DetailActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
